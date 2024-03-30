0fa4c8c4644d0e4330452b0ca7d037a182820e73
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-30 20:22:32 +0800

    add: functions

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    class User {
+
+        // properties methods
+        public $username = 'Mark';
+
+    }
+
+    $userOne = new User();
+
+    echo $userOne->username;
+
+    // echo get_class($userOne);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP oop tutorial</title>
+    </head>
+    <body>
+
+    </body>
+</html>
