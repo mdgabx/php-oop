6ec44ac3a820b72961c387ea48def0e4d0c716d4
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-03-31 20:29:52 +0800

    add method

--- index.php
+++ index.php
@@ -5,15 +5,19 @@
         // properties methods
         public $username = 'Mark';
 
+        public function showMessage() {
+            return "$this->username is ma name";
+        }
+
     }
 
     $userOne = new User();
 
-    echo $userOne->username;
+    echo $userOne->showMessage();
 
-    // echo get_class($userOne);
-    print_r(get_class_vars('User'));
-    print_r(get_class_methods('User'));
+    // // echo get_class($userOne);
+    // print_r(get_class_vars('User'));
+    // print_r(get_class_methods('User'));
 ?>
 
 <!DOCTYPE html>
